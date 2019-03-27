f4619b67831c1e55ea9051b65e1a0c2ab9bf9d5a
Monica <monica.k@spi-global.com>
2019-03-27 18:51:15 +0530

    Firebase Setup

--- Flash Chat/Controller/AppDelegate.swift
+++ Flash Chat/Controller/AppDelegate.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import CoreData
-
+import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -17,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
+        
+        let myDatabase = Database.database().reference()
+        myDatabase.setValue("We've got data!")
         return true
     }
 
